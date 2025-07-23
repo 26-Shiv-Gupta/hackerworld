bfde607db6fdc6d4b59df274387a6a8aced2c4f1
26-Shiv-Gupta <gedashiv9977@gmail.com>
2025-07-23 16:54:55 +0530

    error_resolve

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
 import Hero_section from '../components/Hero_section'
 import Stats_section from '../components/Stats_section'
-import Courses_section from '../components/courses_section'
 import Features_section from '../components/Features_section'
 import Testimonial_section from '../components/Testimonial_section'
+import Courses_section from '../components/Courses_section'
 import CTA_section from '../components/CTA_section'
-import Footer from '../components/Footer'
-import Contact from './Contacts'
-import Career from './Careers'
 
 const Home = () => {
   return (
