a59f704efa3c10029c4d2f7752311cd4d72763b4
Shiv Gupta <98166888+26-Shiv-Gupta@users.noreply.github.com>
2025-08-11 08:37:08 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -22,6 +22,5 @@ app.use('/api/courses', require('./routes/courseRoutes'))
 app.use('/api/careers', require('./routes/careerRoutes'))
 app.use('/api/homeCourses', require('./routes/homeCoursesRoutes'))
 app.use('/api/homeReviews', require('./routes/homeReviewsRoutes'))
-app.us
 
 app.listen(port, () => console.log(`server started on port ${port}`))
