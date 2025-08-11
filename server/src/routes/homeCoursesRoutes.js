f12907ef8b7f164623d7d089d51382ee07985517
26-Shiv-Gupta <gedashiv9977@gmail.com>
2025-08-11 08:18:21 +0530

    home courses controller bug fix

--- server/src/routes/homeCoursesRoutes.js
+++ server/src/routes/homeCoursesRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const { gethomeCourses, sethomeCourse, updatehomeCourse, deletehomeCourse } = require('../controllers/homecoursesController')
+const { gethomeCourses, sethomeCourse, updatehomeCourse, deletehomeCourse } = require('../controllers/homeCoursesController')
 
 router.route('/').get(gethomeCourses).post(sethomeCourse)
 router.route('/:id').put(updatehomeCourse).delete(deletehomeCourse)
