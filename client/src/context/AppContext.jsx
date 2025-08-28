20da7d68c1b675ac60351ce409104e8392d4999b
26-Shiv-Gupta <gedashiv9977@gmail.com>
2025-08-28 22:39:07 +0530

    app context update

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,68 +1,83 @@
 // src/context/AppContext.jsx
 import { createContext, useEffect, useState } from "react";
+import { useAuth, useUser } from "@clerk/clerk-react";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
-  const [courses, setCourses] = useState([]);
-  const [testimonials, setTestimonials] = useState([]);
-  const [careers, setCareers] = useState([]);
 
-  // Fetch all courses
-  const fetchCourses = async () => {
-    try {
-      const res = await fetch("https://hackerworld.onrender.com/api/courses");
-      const data = await res.json();
-      setCourses(data);
-    } catch (error) {
-      console.error("Failed to fetch courses:", error);
+    const { getToken } = useAuth()
+    const { user } = useUser()
+
+    const [courses, setCourses] = useState([]);
+    const [testimonials, setTestimonials] = useState([]);
+    const [careers, setCareers] = useState([]);
+
+    // Fetch all courses
+    const fetchCourses = async () => {
+        try {
+            const res = await fetch("https://hackerworld.onrender.com/api/courses");
+            const data = await res.json();
+            setCourses(data);
+        } catch (error) {
+            console.error("Failed to fetch courses:", error);
+        }
+    };
+
+    // Fetch testimonials
+    const fetchTestimonials = async () => {
+        try {
+            const res = await fetch("https://hackerworld.onrender.com/api/homeReviews");
+            const data = await res.json();
+            setTestimonials(data);
+        } catch (error) {
+            console.error("Failed to fetch testimonials:", error);
+        }
+    };
+
+    // Fetch testimonials
+    const fetchCareers = async () => {
+        try {
+            const res = await fetch("https://hackerworld.onrender.com/api/careers");
+            const data = await res.json();
+            setCareers(data);
+        } catch (error) {
+            console.error("Failed to fetch testimonials:", error);
+        }
+    };
+
+    // Fetch all on mount
+    useEffect(() => {
+        fetchCourses();
+        fetchTestimonials();
+        fetchCareers();
+    }, []);
+
+    const logToken = async () => {
+        console.log(await getToken());
     }
-  };
+    
+    useEffect(() => {
+        if (user) {
+            logToken()
+        }
+    }, [user])
 
-  // Fetch testimonials
-  const fetchTestimonials = async () => {
-    try {
-      const res = await fetch("https://hackerworld.onrender.com/api/homeReviews");
-      const data = await res.json();
-      setTestimonials(data);
-    } catch (error) {
-      console.error("Failed to fetch testimonials:", error);
-    }
-  };
-
-  // Fetch testimonials
-  const fetchCareers = async () => {
-    try {
-      const res = await fetch("https://hackerworld.onrender.com/api/careers");
-      const data = await res.json();
-      setCareers(data);
-    } catch (error) {
-      console.error("Failed to fetch testimonials:", error);
-    }
-  };
-
-  // Fetch all on mount
-  useEffect(() => {
-    fetchCourses();
-    fetchTestimonials();
-    fetchCareers();
-  }, []);
-
-  return (
-    <AppContext.Provider
-      value={{
-        courses,
-        setCourses,
-        testimonials,
-        setTestimonials,
-        careers,
-        setCareers,
-        fetchTestimonials,
-        fetchCourses,
-        fetchCareers,
-      }}
-    >
-      {children}
-    </AppContext.Provider>
-  );
+    return (
+        <AppContext.Provider
+            value={{
+                courses,
+                setCourses,
+                testimonials,
+                setTestimonials,
+                careers,
+                setCareers,
+                fetchTestimonials,
+                fetchCourses,
+                fetchCareers,
+            }}
+        >
+            {children}
+        </AppContext.Provider>
+    );
 };
