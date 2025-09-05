cf15c5b9eac272ecab9a209a5d07f7b2803d1235
26-Shiv-Gupta <gedashiv9977@gmail.com>
2025-09-05 09:28:46 +0530

    bug update

--- client/src/pages/Payment.jsx
+++ client/src/pages/Payment.jsx
@@ -1,6 +1,6 @@
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-import CheckoutForm from "../components/checkoutForm  ";
+import CheckoutForm from "../components/checkoutForm";
 
 const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
 
