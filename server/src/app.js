a3b010a278577e325b235d39515b8f8123ef49d1
26-Shiv-Gupta <gedashiv9977@gmail.com>
2025-09-07 03:15:08 +0530

    cors

--- server/src/app.js
+++ server/src/app.js
@@ -12,7 +12,11 @@ const app = express();
 
 // CORS - allow Authorization header from frontend as well
 app.use(cors({
-  origin: 'https://techhackworld.onrender.com',
+  origin: [
+    'https://techhackworld.onrender.com',
+    "https://hackerworld.onrender.com",
+    "http://localhost:5173"
+  ],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
