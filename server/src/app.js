6062fd1858798f24364012c10a364e10f4176b99
26-Shiv-Gupta <gedashiv9977@gmail.com>
2026-06-25 06:15:00 +0530

    cors allow changes

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,11 @@
-// server/src/app.js
-const express = require('express');
-const cors = require('cors');
+const express = require('express'); // Express framework import kar rahe hain. Server aur APIs banane ke liye use hota hai.
+const cors = require('cors'); // CORS (Cross-Origin Resource Sharing) middleware. Frontend aur backend alag domains/ports par ho to requests allow karta hai.
 require('dotenv').config();
-const connectDB = require('./config/db');
-const { clerkWebhooks } = require('./controllers/webhooks');
-const educatorRouter = require('./routes/educatorRoutes');
-const { clerkMiddleware } = require('@clerk/express');
+const connectDB = require('./config/db'); // MongoDB se connection establish karne wala custom function.
+const { clerkWebhooks } = require('./controllers/webhooks'); // Clerk webhook controller. Clerk se aane wale events (user create, delete, update) handle karega.
+const educatorRouter = require('./routes/educatorRoutes'); // Educator-related routes. Teacher/educator ke APIs yahan define hongi.
+const { clerkMiddleware } = require('@clerk/express'); // Clerk authentication middleware. User login/authentication verify karta hai.
+const paymentRoutes = require('./routes/paymentRoutes') // Payment-related routes. Payment create, verify, success, failure etc. handle karenge.
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -15,7 +15,8 @@ app.use(cors({
   origin: [
     'https://techhackworld.onrender.com',
     "https://hackerworld.onrender.com",
-    "http://localhost:5173"
+    "http://localhost:5173", 
+    "https://hackerworld-admin.onrender.com/"
   ],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
@@ -26,9 +27,6 @@ connectDB();
 
 // Clerk middleware
 app.use(clerkMiddleware());
-
-// IMPORTANT: mount stripe webhook BEFORE body parsers so stripe can access raw body for signature verification
-app.use('/api', require('./routes/stripeWebhook'));
 
 // Body parsers for all other routes
 app.use(express.json());
@@ -41,6 +39,6 @@ app.use('/api/courses', require('./routes/courseRoutes'));
 app.use('/api/careers', require('./routes/careerRoutes'));
 app.use('/api/homeCourses', require('./routes/homeCoursesRoutes'));
 app.use('/api/homeReviews', require('./routes/homeReviewsRoutes'));
-app.use('/api/payment', require('./routes/paymentRoutes'));
+app.use("/api/payment", paymentRoutes);
 
 app.listen(port, () => console.log(`server started on port ${port}`));
