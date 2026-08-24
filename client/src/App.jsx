10ebad53ed44b7a79399ac34f9dce0021a79ec52
26-Shiv-Gupta <gedashiv9977@gmail.com>
2026-08-24 10:38:35 +0530

    Scroll to top

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
 
   return (
     <>
+      <ScrollToTop />
+
       <Navbar />
       <main className="min-h-screen bg-gray-900 text-white">
         <Outlet />
